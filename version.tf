f3125ec603beb599fef3c468f315a1764c7a74a8
JoDaniel1412 <jodaniel1409@gmail.com>
2023-04-16 20:56:26 -0600

    Updated VMs resource

--- version.tf
+++ version.tf
@@ -10,6 +10,10 @@ terraform {
 }
 
 provider "azurerm" {
-  #  skip_provider_registration = "true"
-  features {}
+  skip_provider_registration = "true"
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
--- vms.tf
+++ vms.tf
@@ -1,69 +1,81 @@
 
 # Virtual Machine for the dns
-resource "azurerm_linux_virtual_machine" "dns-vm" {
+resource "azurerm_virtual_machine" "dns-vm" {
   name                = "${var.group}-vm-dns"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  size                = "Standard_B1s"
-
-  admin_username                  = var.vm_cred.user
-  admin_password                  = var.vm_cred.pass
-  disable_password_authentication = "false"
+  vm_size             = "Standard_B1s"
 
   network_interface_ids = [
     azurerm_network_interface.neti.*.id[0],
   ]
 
-  os_disk {
-    name                 = "${var.group}-osdisk-dns"
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
   # OS in wich the VM runs
-  source_image_reference {
+  storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "18.04-LTS"
     version   = "latest"
   }
+
+  storage_os_disk {
+    name              = "${var.group}-osdisk-dns"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      key_data = file("./ssh/id_rsa.pub")
+      path     = "/home/${var.vm_cred.user}/.ssh/authorized_keys"
+    }
+  }
+  os_profile {
+    computer_name  = var.group
+    admin_username = var.vm_cred.user
+  }
 }
 
 # Virtual Machine for the Apche servers
-resource "azurerm_linux_virtual_machine" "apache-vm" {
+resource "azurerm_virtual_machine" "apache-vm" {
   count               = 2
   name                = "${var.group}-vm-apache${count.index + 1}"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  size                = "Standard_B1s"
-
-  admin_username                  = var.vm_cred.user
-  admin_password                  = var.vm_cred.pass
-  disable_password_authentication = "false"
+  vm_size             = "Standard_B1s"
 
   network_interface_ids = [
     azurerm_network_interface.neti.*.id[count.index + 1],
   ]
 
-  os_disk {
-    name                 = "${var.group}-osdisk-apache${count.index + 1}"
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
   # OS in wich the VM runs
-  source_image_reference {
+  storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "18.04-LTS"
     version   = "latest"
   }
 
-  # Update packages and install apache
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update",
-      "sudo apt-get install apache2 -y",
-    ]
+  storage_os_disk {
+    name              = "${var.group}-osdisk-apache${count.index + 1}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  # Certificates
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      key_data = file("./ssh/id_rsa.pub")
+      path     = "/home/${var.vm_cred.user}/.ssh/authorized_keys"
+    }
+  }
+
+  os_profile {
+    computer_name  = var.group
+    admin_username = var.vm_cred.user
   }
 }
